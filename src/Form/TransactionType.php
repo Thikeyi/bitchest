abad00a8a13047d4f39806a14664523e92cac04a
Annie <anniedogo@gmail.com>
2019-10-31 15:03:06 +0100

    Transaction form

---
+++ src/Form/TransactionType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Transaction;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TransactionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('transactionDate')
+            ->add('quantity')
+            ->add('price')
+            ->add('amount')
+            ->add('type')
+            ->add('currency')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Transaction::class,
+        ]);
+    }
+}
